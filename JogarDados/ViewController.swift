73f71881d7f7c6d95c6a989a06995115819d8934
d-llirium <plimacarlos@gmail.com>
2021-05-07 09:58:27 -0300

    create patricia's file

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: - OUTLETS
+    @IBOutlet weak var dadoImageView01: UIImageView!
+    @IBOutlet weak var dadoImageView02: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        dadoImageView01.image = #imageLiteral(resourceName: "DiceFour")
+        dadoImageView01.alpha = 0.5
     }
-
 
 }
 
