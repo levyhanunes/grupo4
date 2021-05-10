37a7b208ea4414007c5e57ad8d9240b421db07ac
d-llirium <plimacarlos@gmail.com>
2021-05-10 17:06:14 -0300

    roll the dices from 1 to 6

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -8,14 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //MARK: - OUTLETS
+    //MARK: - OUTLETS: não requer ação do usuário
     @IBOutlet weak var dadoImageView01: UIImageView!
     @IBOutlet weak var dadoImageView02: UIImageView!
+    var selectDado1 = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        dadoImageView01.image = #imageLiteral(resourceName: "DiceFour")
-        dadoImageView01.alpha = 0.5
+        //who.what = value
+        //>dadoImageView01.image = #imageLiteral(resourceName: "DiceFour")
     }
-
+    //MARK: - ACTION: requer interação da interface com o código através de uma ação do usuário
+    @IBAction func rolarDadosPressed(_ sender: UIButton) {
+        //para testar
+        //>print("Botão Rolar Dados Pressionado!!!")
+        //Exercícios: Fazer com que quando precione o botao rolar dados os dois dados estejam no numero 4.
+        //>dadoImageView01.image = #imageLiteral(resourceName: "DiceFour")
+        //>dadoImageView02.image = #imageLiteral(resourceName: "DiceFour")
+        //cada vez q clica no botão aumenta um
+        dadoImageView01.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][selectDado1]
+        dadoImageView02.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][selectDado1]
+        selectDado1 += 1
+        if (selectDado1 > 5){
+            selectDado1 = 0
+        }
+    }
 }
 
